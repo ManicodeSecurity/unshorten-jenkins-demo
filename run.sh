3754c70f041928c010fa4df6dfada9bc2244ad82
Jimmy Mesta <jimmy@mestamachine.io>
2018-03-25 14:18:36 -0700

    remove run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://github.com/arminc/clair-scanner/releases/download/v5/clair-scanner_linux_amd64 --no-check-certificate
-mv clair-scanner_linux_amd64 clair-scanner
-chmod +x clair-scanner
